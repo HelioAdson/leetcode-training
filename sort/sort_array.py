2d266f35aea41810cd0736aaf7dcc13c63ed7b1e
HelioAdson <helioadson@gmail.com>
2024-08-03 13:21:56 -0300

    912 - sort an array - merge sort

--- sort/sort_array.py
+++ sort/sort_array.py
@@ -21,3 +21,36 @@ class Solution(object):
         major_values.append(i)
 
     return self.sortArray(lowest_values) + equal_values + self.sortArray(major_values)
+
+
+# merge sort
+
+class Solution(object):
+  def sortArray(self, nums):
+    #base case
+    if len(nums) < 2:
+      return nums
+
+    mid = len(nums) // 2
+    left_array = self.sortArray(nums[:mid])
+    right_array = self.sortArray(nums[mid:])
+    return self.merge(left_array, right_array)
+
+  def merge(self, left_array, right_array):
+    sorted_array = []
+    i = 0
+    j = 0
+
+    while i < len(left_array) and j < len(right_array):
+      if left_array[i] < right_array[j]:
+        sorted_array.append(left_array[i])
+        i += 1
+      else:
+        sorted_array.append(right_array[j])
+        j += 1
+
+    # remaining elements
+    sorted_array = sorted_array + left_array[i:]
+    sorted_array = sorted_array + right_array[j:]
+
+    return sorted_array
